2ee9e22ecf818a31c3cc90a9f5dce6cf98591735
Thiago Mendes <thiago.mendes@dataseed.de>
2020-10-14 18:17:08 -0300

    Remoção de algumas classes de teste

--- src/teste/TesteInjector.java
+++
@@ -1,9 +0,0 @@
-import com.google.inject.AbstractModule;
-
-public class TesteInjector extends AbstractModule{
-
-    @Override
-    protected void configure() {
-        bind(TesteService.class).to(Testinho.class);
-    }
-}
